eb8c9064f75024b53603cb58a049e9c73829e238
Kim-WonJoon <skygood96@gmail.com>
2025-02-12 20:24:39 +0900

    first commit

---
+++ src/main/java/com/example/schedule_developlv1/ScheduleDevelopLv1Application.java
@@ -0,0 +1,13 @@
+package com.example.schedule_developlv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelopLv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopLv1Application.class, args);
+    }
+
+}
